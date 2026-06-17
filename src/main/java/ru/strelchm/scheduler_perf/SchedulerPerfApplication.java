e5f60eff6731899424ecb9006616c1f7d1504320
strelchm <strelchm@yandex.ru>
2026-06-18 00:17:58 +0700

    initial spring project is added

---
+++ src/main/java/ru/strelchm/scheduler_perf/SchedulerPerfApplication.java
@@ -0,0 +1,13 @@
+package ru.strelchm.scheduler_perf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerPerfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulerPerfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/strelchm/scheduler_perf/SchedulerPerfApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.strelchm.scheduler_perf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerPerfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
