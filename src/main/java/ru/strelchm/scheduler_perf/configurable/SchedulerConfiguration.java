a22313084ec8804735fed77328199dac4302cbc8
strelchm <strelchm@yandex.ru>
2026-06-29 12:03:38 +0700

    db-scheduler is added

---
+++ src/main/java/ru/strelchm/scheduler_perf/configurable/SchedulerConfiguration.java
@@ -0,0 +1,50 @@
+package ru.strelchm.scheduler_perf.configurable;
+
+import com.github.kagkarlsson.scheduler.CurrentlyExecuting;
+import com.github.kagkarlsson.scheduler.boot.autoconfigure.Jackson3Serializer;
+import com.github.kagkarlsson.scheduler.boot.config.DbSchedulerCustomizer;
+import com.github.kagkarlsson.scheduler.event.AbstractSchedulerListener;
+import com.github.kagkarlsson.scheduler.event.SchedulerListener;
+import com.github.kagkarlsson.scheduler.serializer.Serializer;
+import com.github.kagkarlsson.scheduler.task.ExecutionComplete;
+import com.github.kagkarlsson.scheduler.task.TaskInstance;
+import org.slf4j.MDC;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Optional;
+
+@Configuration
+public class SchedulerConfiguration {
+
+    private static final String MDC_TASK_NAME = "task-name";
+    private static final String MDC_TASK_INSTANCE_ID = "task-instance-id";
+
+    @Bean
+    DbSchedulerCustomizer customizer() {
+        return new DbSchedulerCustomizer() {
+            @Override
+            public Optional<Serializer> serializer() {
+                return Optional.of(new Jackson3Serializer());
+            }
+        };
+    }
+
+    @Bean
+    SchedulerListener mdcSchedulerListener() {
+        return new AbstractSchedulerListener() {
+            @Override
+            public void onExecutionStart(CurrentlyExecuting executing) {
+                TaskInstance<?> taskInstance = executing.getTaskInstance();
+                MDC.put(MDC_TASK_NAME, taskInstance.getTaskName());
+                MDC.put(MDC_TASK_INSTANCE_ID, taskInstance.getId());
+            }
+
+            @Override
+            public void onExecutionComplete(ExecutionComplete executionComplete) {
+                MDC.remove(MDC_TASK_NAME);
+                MDC.remove(MDC_TASK_INSTANCE_ID);
+            }
+        };
+    }
+}
