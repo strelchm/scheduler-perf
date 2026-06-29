a71d9fa2e54446e475f34c53f19966972ab2e8a8
strelchm <strelchm@yandex.ru>
2026-06-29 13:57:55 +0700

    jobrunr is added; config profiles are separated

--- src/main/java/ru/strelchm/scheduler_perf/configurable/DbSchedulerConfiguration.java
+++ src/main/java/ru/strelchm/scheduler_perf/configurable/DbSchedulerConfiguration.java
@@ -11,11 +11,13 @@ import com.github.kagkarlsson.scheduler.task.TaskInstance;
 import org.slf4j.MDC;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import java.util.Optional;
 
 @Configuration
-public class SchedulerConfiguration {
+@Profile("db-scheduler")
+public class DbSchedulerConfiguration {
 
     private static final String MDC_TASK_NAME = "task-name";
     private static final String MDC_TASK_INSTANCE_ID = "task-instance-id";
