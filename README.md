5b095410eb9ed2c97170e6fbab7e1d6ab12ee333
strelchm <strelchm@yandex.ru>
2026-07-05 00:15:35 +0700

    generic db-scheduler mode is added

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 # scheduler-perf
-Comparing persistent scheduling libs with load testing
+Comparing persistent scheduling libs performance with load testing
 
 Modes
 ===================
-For db-scheduler enabling db-scheduler profile activating is needed, for example with ENV-s:
-`spring.profiles.active=db-scheduler`
-JobRunr activating:
-`spring.profiles.active=jobrunr`
+For JobRunr enabling jobrunr profile activating is needed, for example with ENV-s:
+```
+spring.profiles.active=jobrunr
+```
+For db-scheduler enabling use:
+```
+spring.profiles.active=db-scheduler
+```
+For special generic lock and fetch mode testing several profile activation is needed:
+```
+spring.profiles.active=db-scheduler,db-scheduler-generic
+```
+
 
--- src/main/java/ru/strelchm/scheduler_perf/configurable/DbSchedulerConfiguration.java
+++ src/main/java/ru/strelchm/scheduler_perf/configurable/DbSchedulerConfiguration.java
@@ -5,16 +5,21 @@ import com.github.kagkarlsson.scheduler.boot.autoconfigure.Jackson3Serializer;
 import com.github.kagkarlsson.scheduler.boot.config.DbSchedulerCustomizer;
 import com.github.kagkarlsson.scheduler.event.AbstractSchedulerListener;
 import com.github.kagkarlsson.scheduler.event.SchedulerListener;
+import com.github.kagkarlsson.scheduler.jdbc.JdbcCustomization;
+import com.github.kagkarlsson.scheduler.jdbc.PostgreSqlJdbcCustomization;
 import com.github.kagkarlsson.scheduler.serializer.Serializer;
 import com.github.kagkarlsson.scheduler.task.ExecutionComplete;
 import com.github.kagkarlsson.scheduler.task.TaskInstance;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import java.util.Optional;
 
+@Slf4j
 @Configuration
 @Profile("db-scheduler")
 public class DbSchedulerConfiguration {
@@ -23,11 +28,18 @@ public class DbSchedulerConfiguration {
     private static final String MDC_TASK_INSTANCE_ID = "task-instance-id";
 
     @Bean
-    DbSchedulerCustomizer customizer() {
+    DbSchedulerCustomizer customizer(@Value("${db-scheduler.generic-lock-and-fetch:false}") Boolean genericLockAndFetch) {
         return new DbSchedulerCustomizer() {
             @Override
             public Optional<Serializer> serializer() {
                 return Optional.of(new Jackson3Serializer());
+            }
+
+            @Override
+            public Optional<JdbcCustomization> jdbcCustomization() {
+                // different modes, see https://github.com/kagkarlsson/db-scheduler/issues/823
+                log.info("LockAndFetch is {}", (genericLockAndFetch ? "generic" : "single statement"));
+                return Optional.of(new PostgreSqlJdbcCustomization(genericLockAndFetch, false));
             }
         };
     }
